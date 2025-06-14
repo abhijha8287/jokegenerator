c962d3bf1e19e28ec54b950f2e6c9c9b9ca1052b
abhishek <abhijha8287@gmail.com>
2025-06-14 10:56:03 +0530

    first commit

---
+++ passthrwrunable.py
@@ -0,0 +1,34 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+import streamlit as st
+load_dotenv()
+st.header("Joke Generator")
+prompt1 = PromptTemplate(
+    template='Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI(model_name="gpt-4", api_key=st.secrets['api_key'], temperature=0)
+
+parser = StrOutputParser()
+
+prompt2 = PromptTemplate(
+    template='Explain the following joke - {text}',
+    input_variables=['text']
+)
+topic=st.text_input("Enter your topic")
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'Joke': RunnablePassthrough(),
+    'Explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+if st.button('Generate Joke'):
+    st.write(final_chain.invoke({topic}))
+
+    
